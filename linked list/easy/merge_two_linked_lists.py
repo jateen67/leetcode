e98873998a769ce9078b45976e635d973d0e8628
exit <kalsijatin67@gmail.com>
2022-08-15 00:56:26 -0400

    .

---
+++ linked list/easy/merge_two_linked_lists.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def mergeTwoLists(l1, l2):
+    dummy = Node()
+    tail = dummy
+
+    while l1 and l2:
+        if l1.val <= l2.val:
+            tail.next = l1
+            l1 = l1.next
+        else:
+            tail.next = l2
+            l2 = l2.next
+        tail = tail.next
+
+    if l1:
+        tail.next = l1
+    if l2:
+        tail.next = l2
+    printList(dummy.next)
+
+
+def printList(head):
+    curr = head
+    while curr:
+        print(curr.val)
+        curr = curr.next
+
+
+n = Node(1)
+n.next = Node(2)
+n.next.next = Node(4)
+n2 = Node(1)
+n2.next = Node(3)
+n2.next.next = Node(4)
+print(mergeTwoLists(n, n2))
+
+# time complexity: o(n)
+# space complexity: o(n)
---
+++ linked list/easy/reverse_linked_list.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def reverseLinkedList(head):
+    prev = None
+    curr = head
+
+    while curr:
+        tmp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = tmp
+    printList(prev)
+
+
+def printList(head):
+    curr = head
+    while curr:
+        print(curr.val)
+        curr = curr.next
+
+
+n = Node(1)
+n.next = Node(2)
+n.next.next = Node(3)
+n.next.next.next = Node(4)
+n.next.next.next.next = Node(5)
+print(reverseLinkedList(n))
+
+# time complexity: o(n)
+# space complexity: o(1)
