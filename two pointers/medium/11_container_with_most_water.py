0905b5ad8443a3e5b3039a5195cdcbafc77dfa4f
exit <kalsijatin67@gmail.com>
2022-08-25 01:03:02 -0400

    .

--- two pointers/medium/11_container_with_most_water.py
+++ two pointers/medium/11_container_with_most_water.py
@@ -1,2 +1,20 @@
 def maxArea(height):
-    pass
+    l = 0
+    r = len(height) - 1
+    area = 0
+    while l < r:
+        h = min(height[l], height[r])
+        width = r - l
+        if h * width > area:
+            area = h * width
+        if height[l] < height[r]:
+            l += 1
+        elif height[l] >= height[r]:
+            r -= 1
+    return area
+
+
+print(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+
+# time complexity: o(n)
+# space complexity: o(1)
