003324c03c5e9e6f6a351e4d57169e3e5c498471
exit <kalsijatin67@gmail.com>
2023-08-25 08:16:45 -0400

    .

--- arrays and hashing/easy/27_remove_element.py
+++ arrays and hashing/easy/27_remove_element.py
@@ -1,8 +1,12 @@
 def removeElement(nums, val):
-    r = len(nums) - 1
-    res = 0
+    k = 0
 
-    return res - len(nums)
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[k] = nums[i]
+            k += 1
+
+    return k
 
 
 print(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
